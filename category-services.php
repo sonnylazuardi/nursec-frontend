8a17c4ee08180caa88a4fc004aed3bb2bf1f3df2
alifradityar <alifradityar@gmail.com>
2015-09-20 20:17:30 +0700

    rata

---
+++ category-services.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+	<main role="main">
+		<!-- section -->
+		<section>
+			<div class="container">
+				<?php get_template_part('loop'); ?>
+
+				<?php get_template_part('pagination'); ?>
+			</div>
+
+		</section>
+		<!-- /section -->
+	</main>
+
+<?php get_footer(); ?>
